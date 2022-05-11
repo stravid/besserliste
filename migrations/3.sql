262bbedb5ac6bd3c7a9cea3f5689852904b29cb2
David Strauß <david.strauss@edgycircle.com>
2022-05-11 07:31:44 +0200

    Fix migration that uses `PRAGMA foreign_keys`
    This PRAGMA is a no-op inside transactions therefore the migration
    does not work. To accommondate our migration logic we have to
    commit/begin the wrapping transaction. A very dirty solution but
    currently the only fast solution I can think of.
    
    In the future a migration script should probably control the logic
    around transactions. Atleast that is what other tools seem to be
    doing.

--- migrations/3.sql
+++ migrations/3.sql
@@ -1,3 +1,9 @@
+-- Commit outer transaction so we can change the foreign_keys PRAGMA
+COMMIT;
+
+PRAGMA foreign_keys=OFF;
+BEGIN;
+
 CREATE TABLE categories_products (
   category_id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
@@ -9,7 +15,6 @@ CREATE UNIQUE INDEX idx_categories_products ON categories_products(category_id, 
 
 INSERT INTO categories_products (category_id, product_id) SELECT category_id, id FROM products;
 
-PRAGMA foreign_keys=OFF;
 CREATE TABLE new_products (
   id INTEGER PRIMARY KEY,
   name_singular TEXT NOT NULL CHECK(length(name_singular) <= 40) COLLATE de_AT,
@@ -20,10 +25,7 @@ DROP TABLE products;
 ALTER TABLE new_products RENAME TO products;
 CREATE UNIQUE INDEX idx_products_name_singular ON products(lower(name_singular, 'de_AT'));
 CREATE UNIQUE INDEX idx_products_name_plural ON products(lower(name_plural, 'de_AT'));
-PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
 
-PRAGMA foreign_keys=OFF;
 CREATE TABLE new_product_changes (
   id INTEGER PRIMARY KEY,
   product_id INTEGER NOT NULL,
@@ -37,5 +39,11 @@ CREATE TABLE new_product_changes (
 INSERT INTO new_product_changes (id, product_id, user_id, name_singular, name_plural, recorded_at) SELECT id, product_id, user_id, name_singular, name_plural, recorded_at FROM product_changes;
 DROP TABLE product_changes;
 ALTER TABLE new_product_changes RENAME TO product_changes;
+
 PRAGMA foreign_key_check;
+COMMIT;
+
 PRAGMA foreign_keys=ON;
+
+-- Begin outer transaction so the migration logic does not break
+BEGIN;
