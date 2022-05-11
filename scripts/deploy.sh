251e2ae4bcb267b67525fe8caa196be2fdcc37e0
David Strauß <david.strauss@edgycircle.com>
2022-05-11 07:35:15 +0200

    Fix deployment script so it works after the initial deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,11 @@ set -o pipefail
 
 ssh -t deployer@pandora.stravid.com -p 5020 << EOF
   cd ~/code/besserliste
+  git checkout master
+  git pull
   export PATH=$PATH:/usr/local/go/bin
   go build -a -ldflags="-v -extldflags ''" -tags "netgo sqlite_omit_load_extension sqlite_json1 sqlite_icu"
+  sudo systemctl stop besserliste
   cp besserliste ~/apps/besserliste/
   sudo systemctl restart besserliste
 EOF
