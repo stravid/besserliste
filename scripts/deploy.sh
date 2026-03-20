a7400dedb62b029902a7ea09ada26001a80fc878
David Strauß <david@strauss.io>
2026-03-20 19:30:25 +0100

    Fix deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-rm ./besserliste
+rm -f ./besserliste
 go build -tags "icu"
 
 scp ./besserliste deployer@app001.stravid.com:/srv/besserliste/besserliste.tmp
 
-ssh -t deployer@pandora.stravid.com -p 5020 << EOF
-  systemctl stop besserliste
-  rm /srv/besserliste/besserliste
+ssh -t deployer@app001.stravid.com << EOF
+  sudo systemctl stop besserliste
+  rm -f /srv/besserliste/besserliste
   mv /srv/besserliste/besserliste.tmp /srv/besserliste/besserliste
-  systemctl start besserliste
+  sudo systemctl start besserliste
 EOF
+
+rm -f ./besserliste
